58f7941bcb967762f1758fb9c8a38981b30f2e5a
sbrown555 <sbrown@berkeley.edu>
2025-08-22 04:17:30 -0700

    Update gc_graphing_all_data.py

--- gc_graphing_all_data.py
+++ gc_graphing_all_data.py
@@ -246,7 +246,7 @@ for df in list_df:
 
 
 # plt.clf()
-fig, axes = plt.subplots(2,1, figsize = (12,6)))
+fig, axes = plt.subplots(2,1, figsize = (12,6))
 
 ax1 = axes[0]
 ax1.plot(data_a['minute'], data_a['CO2'], 'b-', label='CO2')
