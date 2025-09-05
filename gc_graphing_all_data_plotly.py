22b281d9b8081d370d506b11db98b499308b32cf
sbrown555 <sbrown@berkeley.edu>
2025-09-05 16:04:42 -0700

    Update gc_graphing_all_data_plotly.py

--- gc_graphing_all_data_plotly.py
+++ gc_graphing_all_data_plotly.py
@@ -90,6 +90,80 @@ def data_from_date(date_folder, actual, time_offset = None, cols = None):
   data.dropna(how='any', inplace=True)
   return data
 
+def data_from_ethernet(date_folder, actual, time_offset = None, cols = None):
+  if 'ethernet' not in date_folder.name:
+    df = pd.DataFrame()
+    return df
+  else:
+    if time_offset is None:
+      time_offset = [pd.Timedelta(days=0), pd.Timedelta(days=0)]
+    if not isinstance(actual, bool):
+      raise ValueError('actual must be True or False')
+    elif actual == True:
+      string_id = 'actual'
+    elif actual == False:
+      string_id = 'sp'
+    
+
+
+
+
+
+processed_files = set()
+  list_df=[]
+  date_folder_id = date_folder['id']
+  for chamber_folder in drive.ListFile({'q':f"'{date_folder_id}' in parents and trashed=false"}).GetList():
+    if 'other' not in chamber_folder['title']:
+      # print(f"chamber folder {string_id}: {chamber_folder['title']}")
+      st.write(f"chamber folder {string_id}: {chamber_folder['title']}")
+      chamber_folder_id = chamber_folder['id']
+      chamber = chamber_folder['title'].split('/')[-1].split('_')[0][-1]
+      for process_folder in drive.ListFile({'q':f"'{chamber_folder_id}' in parents and trashed=false"}).GetList():
+        process_folder_id = process_folder['id']
+        for process in processes.keys():
+          for csv_file in drive.ListFile({'q':f"'{process_folder_id}' in parents and trashed=false"}).GetList():
+            if csv_file['title'].startswith('._'):
+              continue  # skip macOS hidden files
+            if csv_file['title'].split('.')[-1] == 'csv' and process in csv_file['title'] and string_id in csv_file['title']:
+              if csv_file['title'] not in processed_files:
+                processed_files.add(csv_file['title'])
+                csv_id = csv_file['id']
+                file = drive.CreateFile({'id': csv_id})
+                csv_content = file.GetContentString()  # returns the raw CSV text
+                csv_file = StringIO(csv_content)
+                try:
+                  try:
+                    df = pd.read_csv(csv_file, usecols = cols)
+                  except UnicodeDecodeError:
+                    df = pd.read_csv(csv_file, encoding='latin1', engine = 'python', on_bad_lines='skip', usecols = cols)
+                  df = data_processing(df)
+                  df['Chamber'] = chamber
+                  df['Process'] = processes[process]
+                  df.reset_index(inplace=True)
+                  list_df.append(df)
+                except Exception as e:
+                  print(f"Failed to read {csv_file['title']}: {e}")
+              else:
+                print(f"already processed {csv_file['title']}")
+  df = pd.concat(list_df, axis=0)
+  df.reset_index(inplace=True)
+  df['minute'] = df['datetime'].dt.strftime('%m/%d/%Y %H')
+  data = df.groupby(['minute', 'Chamber', 'Process']).agg({'Value':'mean'})
+  data.reset_index(inplace=True)
+  data = data.pivot(index=['minute', 'Chamber'], columns = 'Process', values = 'Value')
+  data.reset_index(inplace=True)
+  data.drop(columns = 'PAR_umol', inplace=True)
+  data.rename(columns={'PAR_max':'PAR'}, inplace=True)
+  data['minute'] = pd.to_datetime(data['minute'])
+  data.loc[data['Chamber'] == 'A', 'minute'] = (data.loc[data['Chamber'] == 'A', 'minute'] + time_offset[0])
+  data.loc[data['Chamber'] == 'B', 'minute'] = (data.loc[data['Chamber'] == 'B', 'minute'] + time_offset[1])
+  data.dropna(how='any', inplace=True)
+  return data
+
+
+
+
+
 def read_drive_id(ID, cols = None):
   file = drive.CreateFile({'id': csv_id})
   csv_content = file.GetContentString()  # returns the raw CSV text
