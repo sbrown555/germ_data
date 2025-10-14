89cd05c95b854cc1d980ddff67b17b2a387e09bb
sbrown555 <sbrown@berkeley.edu>
2025-10-14 06:19:20 -0700

    Update gc_graphing_all_data_plotly.py

--- gc_graphing_all_data_plotly.py
+++ gc_graphing_all_data_plotly.py
@@ -336,8 +336,8 @@ df_actual_b = df_actual[df_actual['Chamber'] == 'B']
 # df_sp = data[data['actual_sp'] == 'sp']
 colors = ['#ff9999', '#9999ff']
 colors=['blue', 'red']
-plotly_graph(df_actual_a, df_actual_a, 'PAR', 'CO2', colors=colors, axis_labels = None, legend_labels = None, title='CO2 and PAR in High CO2 Chamber', x_range=None, y_range1=[0,1300], y_range2=[0,1300], units=units, key=None)
-plotly_graph(df_actual_b, df_actual_b, 'PAR', 'CO2', colors=colors, axis_labels = None, legend_labels = None, title='CO2 and PAR in High CO2 Chamber', x_range=None, y_range1=[0,1300], y_range2=[0,1300], units=units, key=None)
+plotly_graph(df_actual_a, df_actual_a, 'PAR', 'CO2', colors=colors, axis_labels = None, legend_labels = None, title='CO2 and PAR in High CO2 Chamber', x_range=None, y_range1=[0,1350], y_range2=[0,1350], units=units, key=None)
+plotly_graph(df_actual_b, df_actual_b, 'PAR', 'CO2', colors=colors, axis_labels = None, legend_labels = None, title='CO2 and PAR in High CO2 Chamber', x_range=None, y_range1=[0,1350], y_range2=[0,1350], units=units, key=None)
 
 
 # SP vs actual (comparing setpoint and actual variables for each chamber)
