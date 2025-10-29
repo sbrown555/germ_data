c93f0c0c059c08de643c9eee0623c94abd67d350
sbrown555 <sbrown@berkeley.edu>
2025-10-28 19:30:32 -0700

    Update vwc_graphing.py

--- vwc_graphing.py
+++ vwc_graphing.py
@@ -14,6 +14,10 @@ import plotly.graph_objects as go
 from plotly.subplots import make_subplots
 import re
 
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+import pandas as pd
+
 def google_drive_access_streamlit():
   sa_json_str = st.secrets["SERVICE_ACCOUNT_JSON"]
   client_email = json.loads(sa_json_str)["client_email"]
@@ -33,17 +37,39 @@ def google_drive_access_streamlit():
 
 drive = google_drive_access_streamlit()
 
-def read_drive_id(ID, cols = None):
-  file = drive.CreateFile({'id': ID})
-  csv_content = file.GetContentString()  # returns the raw CSV text
-  csv_file = StringIO(csv_content)
-  try:
-    df = pd.read_csv(csv_file, usecols = cols)
-  except UnicodeDecodeError:
-    df = pd.read_csv(csv_file, encoding='latin1', engine = 'python', on_bad_lines='skip', usecols = cols)
-  return df
+# def read_drive_id(ID, cols = None):
+#   file = drive.CreateFile({'id': ID})
+#   csv_content = file.GetContentString()  # returns the raw CSV text
+#   csv_file = StringIO(csv_content)
+#   try:
+#     df = pd.read_csv(csv_file, usecols = cols)
+#   except UnicodeDecodeError:
+#     df = pd.read_csv(csv_file, encoding='latin1', engine = 'python', on_bad_lines='skip', usecols = cols)
+#   return df
 
 id = '1Nwj8FKqPq5ILa6c0VlYL98QIP-7FNmAi8N8CPK39XjM'
 
 data = read_drive_id(id)
+st.write(data.head())
+
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+import pandas as pd
+
+# Authenticate
+scope = ["https://spreadsheets.google.com/feeds",
+         "https://www.googleapis.com/auth/drive"]
+creds = ServiceAccountCredentials.from_json_keyfile_name("service_account.json", scope)
+client = gspread.authorize(creds)
+
+# Open the Google Sheet
+sheet = client.open_by_key(ID)  # or open_by_url("https://docs.google.com/spreadsheets/...")
+
+# Select the first worksheet
+worksheet = sheet.get_worksheet(0)
+
+# Get all data as DataFrame
+data = pd.DataFrame(worksheet.get_all_records())
+
+import streamlit as st
 st.write(data.head())
